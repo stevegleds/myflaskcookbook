44b430fbbc37cf39c28267d6c3437ac114e9592b
Steve Gledhill <stevegleds@users.noreply.github.com>
2017-02-15 14:25:24 +0000

    15 February 2017 Reviewed progress and now ready for next step : Databases / models: https://www.youtube.com/watch?v=lXeQcIA0Fd4

--- appkurt/views.py
+++ appkurt/views.py
@@ -1,5 +1,5 @@
 from flask import render_template
-from forms import LoginForm
+from forms import LoginForm  # from forms.py file
 from appkurt import app
 
 
@@ -23,6 +23,7 @@ def index():
 
 
 @app.route('/login', methods=['GET', 'POST'])
+
 def login():
     form = LoginForm()
     return render_template("login.html",
